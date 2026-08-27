4f72082f9b5768c1f01180a316aeabd166fa1904
asaadimohammadrasoul1381-cmyk <asaadimohammadrasoul1381@gmail.com>
2026-08-26 17:41:26 -0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/counseling-panel',
+  assetPrefix: '/counseling-panel/',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
